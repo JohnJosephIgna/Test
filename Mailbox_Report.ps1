d0ad62ffb397ec4630b485170b90b0aeecb542cf
JohnJosephIgna <55781213+JohnJosephIgna@users.noreply.github.com>
2020-08-13 16:53:10 +0800

    Create Mailbox_Report.ps1

---
+++ Mailbox_Report.ps1
@@ -0,0 +1,12 @@
+Get-PSSession | Remove-PSSession
+$Office365Credentials  = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Office365credentials -Authentication Basic –AllowRedirection
+Import-PSSession $Session -AllowClobber | Out-Null
+
+$CSVFilename = read-host "Enter File Name "
+write-host("")
+$OutputFile = $CSVFilename + '.csv'
+$OutputPath = 'C:\Users\' + $env:UserName + '\Desktop\' + $Outputfile
+
+Get-Mailbox -ResultSize Unlimited | Select Identity, UserPrincipalName, PrimarySmtpAddress, RecipientTypeDetails | Export-Csv -Path "$Outputpath"
+write-host("Successfully Exported to Desktop") -F Green
