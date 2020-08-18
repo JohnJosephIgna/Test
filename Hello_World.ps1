3b7e899f6befcc486890530debc7fa46d0cd436f
JohnJosephIgna <55781213+JohnJosephIgna@users.noreply.github.com>
2020-08-19 02:40:24 +0800

    Update Hello_World.ps1

--- Hello_World.ps1
+++ Hello_World.ps1
@@ -1,4 +1,7 @@
 ﻿Write-Host "Hello World" -ForegroundColor Green
-Start-Sleep 10
+Start-Sleep 5
+$Variable = Read-Host "Enter Something"
+Write-Host "You Typed $Variable" -ForegroundColor Cyan
+Start-Sleep 5
 Write-Host "Goodbye World" -ForegroundColor Red
 Start-Sleep 10
