c9a3d52c6d5e94e15c0b21c8546f42f50f071904
JohnJosephIgna <55781213+JohnJosephIgna@users.noreply.github.com>
2020-08-19 02:57:33 +0800

    Update Hello_World.ps1

--- Hello_World.ps1
+++ Hello_World.ps1
@@ -5,5 +5,14 @@ Write-Host "You Typed $Variable" -ForegroundColor Cyan
 Start-Sleep 5
 $PathVariable = "C:\Evologic\"
 Write-Host "You File Path is $PathVariable" -ForegroundColor Cyan
+Write-Host "Choose something"
+Write-Host "1 - Yes | 2 - No"
+Write-Host ""
+$Choice = Read-Host "Enter Choice"
+if ($Choice == 1) {
+    Write-Host "You choose Yes"
+} elseif ($Choice == 2) {
+    Write-Host "You choose No"
+}
 Write-Host "Goodbye World" -ForegroundColor Red
 Start-Sleep 10
