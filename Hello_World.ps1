593a6c9729d23055485f13ff8f3655078851f287
JohnJosephIgna <55781213+JohnJosephIgna@users.noreply.github.com>
2020-08-19 02:48:00 +0800

    Update Hello_World.ps1

--- Hello_World.ps1
+++ Hello_World.ps1
@@ -3,5 +3,7 @@ Start-Sleep 5
 $Variable = Read-Host "Enter Something"
 Write-Host "You Typed $Variable" -ForegroundColor Cyan
 Start-Sleep 5
+$PathVariable = "C:\Evologic\"
+Write-Host "You File Path is $Variable" -ForegroundColor Cyan
 Write-Host "Goodbye World" -ForegroundColor Red
 Start-Sleep 10
