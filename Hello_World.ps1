e497dca5f23d3053f8e3679dfa3c5547f0b446ca
JohnJosephIgna <55781213+JohnJosephIgna@users.noreply.github.com>
2020-08-19 02:58:40 +0800

    Update Hello_World.ps1

--- Hello_World.ps1
+++ Hello_World.ps1
@@ -9,9 +9,9 @@ Write-Host "Choose something"
 Write-Host "1 - Yes | 2 - No"
 Write-Host ""
 $Choice = Read-Host "Enter Choice"
-if ($Choice == 1) {
+if ($Choice = 1) {
     Write-Host "You choose Yes"
-} elseif ($Choice == 2) {
+} elseif ($Choice = 2) {
     Write-Host "You choose No"
 }
 Write-Host "Goodbye World" -ForegroundColor Red
